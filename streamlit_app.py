f11f0bd7dec7abafa69b3c508ca2efb04bf4417a
victoremendes <126639056+victoremendes@users.noreply.github.com>
2023-03-01 00:34:27 -0300

    Update streamlit_app.py
    Acerto de erro de digitação "fruit"

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 import pandas
 
-my_fuit_list =  pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list =  pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Parents New Healthy Diner!')
 
